8cdc8fbf55c4ebefc33cb47df8d6b886822986b4
Daniel Bartschies <daniel.bartschies@blackbit.de>
2019-12-14 21:53:12 +0100

    day3: solved part1

--- src/day3.rs
+++ src/day3.rs
@@ -4,11 +4,8 @@ pub mod part1 {
     use std::io::BufReader;
     use std::io::prelude::*;
     use std::collections::HashMap;
-    use std::cell::Cell;
-    // use std::cell::Cell;
-//    use crate::day3::part1::CellState::Empty;
 
-    #[derive(PartialEq, Eq, Hash)]
+    #[derive(Debug, PartialEq, Eq, Hash)]
     struct Point {
         x: i16,
         y: i16
@@ -34,9 +31,11 @@ pub mod part1 {
         let length = instruction.split_off(1).parse::<i16>().unwrap();
        // let direction = instruction.remove(0);
         //let length = instruction;
-        println!("-------------");
+       /* println!("-------------");
         println!("{} {}", x, y);
         println!("{} steps to {}", length, instruction);
+        println!("-------------");
+        println!("");*/
         //direction = direction.to_string();
       // println!("{:?}", instruction);i
         // direction = direction as String;
@@ -50,7 +49,12 @@ pub mod part1 {
                     let point:Point = Point {x:newx, y:newy};
                     let state = fields.get(&point);
                     match state {
-                        Some(_x) => {fields.insert(point, 9 );}
+                        Some(_x) => {
+
+                            if *state.unwrap() != wire {
+                                fields.insert(point, 9 );
+                            }
+                        }
                         None => {
                             fields.insert(point, wire );
                         },
@@ -63,7 +67,11 @@ pub mod part1 {
                     let point:Point = Point {x:newx, y:newy};
                     let state = fields.get(&point);
                     match state {
-                        Some(_x) => {fields.insert(point, 9 );}
+                        Some(_x) => {
+                            if *state.unwrap() != wire {
+                                fields.insert(point, 9 );
+                            }
+                        }
                         None => {
                             fields.insert(point, wire);
                         },
@@ -76,7 +84,11 @@ pub mod part1 {
                     let point:Point = Point {x:newx, y:newy};
                     let state = fields.get(&point);
                     match state {
-                        Some(_x) => {fields.insert(point, 9 );}
+                        Some(_x) => {
+                            if *state.unwrap() != wire {
+                                fields.insert(point, 9 );
+                            }
+                        }
                         None => {
                             fields.insert(point, wire );
                         },
@@ -89,51 +101,59 @@ pub mod part1 {
                     let point:Point = Point {x:newx, y:newy};
                     let state = fields.get(&point);
                     match state {
-                        Some(_x) => {fields.insert(point, 9 );}
+                        Some(_x) => {
+                            if *state.unwrap() != wire {
+                                fields.insert(point, 9 );
+                            }
+                        }
                         None => {
                             fields.insert(point, wire );
                         },
                     }
                 }
             },
-            _ => println!("blahh blahhh"), //wtf? https://jijnasu.in/rust-non-exhaustive-patterns-_-not-covered-pattern-_-not-covered/
+            _ => unreachable!(), //wtf? https://jijnasu.in/rust-non-exhaustive-patterns-_-not-covered-pattern-_-not-covered/
         }
         println!("{} {}", newx, newy);
         (newx, newy, fields)
     }
 
     pub fn run() {
-        let f = File::open("input/input3_small.csv").unwrap();
+        let f = File::open("input/input3.csv").unwrap();
         let file = BufReader::new(&f);
-        let mut x = 0;
-        let mut y = 0;
+
        // let mut newCellState: CellState;
         let mut fields: Field = HashMap::new();
         for (_num, line) in file.lines().enumerate() {
+            let mut x = 0;
+            let mut y = 0;
             let instruction_line  = line.unwrap();
             let instructions = instruction_line.split(',');
             for instruction in instructions.collect::<Vec<&str>>() {
-
                 let new_coords = draw_line(instruction, x, y, fields, _num);
                 let (_x, _y, _fields) = new_coords;
                 x = _x;
                 y = _y;
                 fields = _fields;
-                println!("{} {}", x,y);
+              //  println!("{} {}", x,y);
             }
         }
         let intersect:usize = 9;
 
+        let mut mdist:i16 = 0;
         for (key, val) in fields.iter() {
-
             if val == &intersect {
+                let abs = key.x.abs()+key.y.abs();
+                if mdist == 0 ||  abs < mdist {
+                    mdist = abs;
+                }
+
                 println!("key: {} {} val: {}", key.x, key.y, val);
             }
         }
-
+        println!("minimal distance {}", mdist);
+        //439 is to low
     }
-
-
 }
 
 
